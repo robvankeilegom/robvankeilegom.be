6561b70592cd634f7a9086c865d5c0148f5a38be
Rob <robvankeilegom@hotmail.com>
2017-04-04 15:47:04 +0200

    update welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,29 +2,57 @@
 
 @section('content')
 <div class="welcome">
-  <header>
-    <div class='overlay'>
-      <div class='welcome-title'>Hallo!</div>
-      <p class="intro">Mijn naam is Rob Van Keilegom, een 3de jaars student aan Thomas More: Campus De Nayer.
-        Als student ICT ontwikkel ik software. Verder op de pagina kan u al de projecten bekijken waar ik al aan (mee)gewerkt heb.
-      </p>
+    <header>
+        <div class='overlay'>
+            <div class='welcome-title'>Hallo!</div>
+            <p class="intro">Mijn naam is Rob Van Keilegom, ik ben een 3de jaars student aan Thomas More: Campus De Nayer.
+                Als student ICT ontwikkel ik software. Verder op de pagina kan u al de projecten bekijken waar ik al aan (mee)gewerkt heb.
+            </p>
 
-      <p class="links">
-         <a href="CV" target="_blank"><i class="fa fa-file-text-o"></i> CV </a>
-         <a href="https://gitlab.com/u/RoobieBoobieee/projects" target="_blank"><i class="fa fa-gitlab"></i>  GitLab </a>
-         <a href="https://www.linkedin.com/in/robvankeilegom" target="_blank"><i class="fa fa-linkedin"></i>  LinkedIn </a>
-         <a href="mailto:RobVanKeilegom@hotmail.com"><i class="fa fa-envelope-o"></i> E-Mail </a>
-       </p>
-      </div>
-  </header>
-  <main>
-    <div class="touchscreen"><img src="{{ asset("images/icons/tap.png")}}" alt="tap"/> Tap op een project om extra info te tonen.</div>
-    <div class='projects'>
-      <h1>Projecten</h1>
+            <p class="links">
+                @foreach ($links as $link)
+                    <a href="{{$link->href}}" @if($link->target_blank) target="_blank" @endif ><i class="fa {{ $link->icon }}"></i> {{ $link->text }} </a>
+                @endforeach
+            </p>
+        </div>
+    </header>
+    <main>
+        <div class="touchscreen"><img src="{{ asset("images/icons/tap.png")}}" alt="tap"/> Tap op een project om extra info te tonen.</div>
+        <div class='projects'>
+            <h1>Projecten</h1>
+            @foreach($projects as $project)
+            <div class='project'>
+                <h2> {{ $project->title }}</h2>
+                <div class=\"img\" style=\"background-image: url('{{ asset(project->image) }}');\"></div>
+                <div class=\"layover\">
+                    <p>
+                        <span class=\"label label-info\">{{ $tag }}</span>
+                    </p>
+                </div>
+                <p>{{ $project->description }}</p>
 
-      <h1> </h1>
-    </div>
-  </main>
+                @if (strpos($row['status'], 'Finished') !== false)
+                    <span class=\"label label-success\">Finished</span>
+                @endif
+                @if (strpos($row['status'], 'Discontinued') !== false)
+                    <span class=\"label label-danger\">Discontinued</span>
+                @endif
+                @if (strpos($row['status'], 'WIP') !== false)
+                    <span class=\"label label-warning\">Work in Progress</span>
+                @endif
+
+                if ($row['sourcelink'])
+                <a href=\"" .$row['sourcelink']. "\" target=\"_blank\"><span class=\"label label-primary\"><i class=\"fa fa-code\"></i> Source</span></a>
+                @endif
+                if ($row['livelink'])
+                <a href=\"" .$row['livelink']. "\" target=\"_blank\"><span class=\"label label-primary\"><i class=\"fa fa-globe\"></i> Live Link</span></a>
+                @endif
+
+            </div>
+            @endforeach
+            <h1> </h1>
+        </div>
+    </main>
 </div>
 @endsection
 
