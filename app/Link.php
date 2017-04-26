77dc6e51ad41f92fdcc4a06584f4be688b41e289
Rob <robvankeilegom@hotmail.com>
2017-04-26 18:05:20 +0200

    update link model

--- app/Link.php
+++ app/Link.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Link extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'icon',
+        'href',
+        'target_blank',
+    ];
+
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 }
