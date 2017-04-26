f3a909ad8f287eadb1638e75bacc359f20dc634b
Rob <robvankeilegom@hotmail.com>
2017-04-26 15:37:21 +0200

    update view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,25 +4,24 @@
 <div class="welcome">
     <header>
         <div class='overlay'>
-            <div class='welcome-title'>Hallo!</div>
+            <h1>Hallo!</h1>
             <p class="intro">Mijn naam is Rob Van Keilegom, ik ben een 3de jaars student aan Thomas More: Campus De Nayer.
                 Als student ICT ontwikkel ik software. Verder op de pagina kan u al de projecten bekijken waar ik al aan (mee)gewerkt heb.
             </p>
 
             <p class="links">
                 @foreach ($links as $link)
-                    <a href="{{ $link->href }}" @if($link->target_blank) target="_blank" @endif ><i class="fa {{ $link->icon }}"></i> {{ $link->text }} </a>
+                    <a href="{{ $link->href }}" @if($link->target_blank) target="_blank" @endif ><i class="fa {{ $link->icon }}"></i> {{ $link->title }} </a>
                 @endforeach
             </p>
         </div>
     </header>
     <main>
-        <div class="touchscreen"><img src="{{ asset("images/icons/tap.png")}}" alt="tap"/> Tap op een project om extra info te tonen.</div>
-        <h1>Projecten</h1>
-        <div class='projects'>
+        <h2>Projecten</h2>
+        <div class='projects-slider'>
             @foreach ($projects as $project)
                 <div class='project'>
-                    <h2> {{ $project->title }}</h2>
+                    <h3> {{ $project->title }}</h3>
                       <div class="project-header">
                         <div class="img" style="background-image: url('{{ $project->tumbnail_image }}');"></div>
                         <div class="layover">
@@ -52,59 +51,21 @@
               </div>
             @endforeach
         </div>
-        <h1> </h1>
+        <a href="#" class='view-more'>See more</a>
+        <h2>Testimonials</h2>
+        <h2>Contact</h2>
+        <h2> </h2>
     </main>
 </div>
 
 <script>
- $( function() {
-$('.project h2').matchHeight();
-$('.projects').slick({
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-});
- });
-// $( function() {
-//   $(".layover").hide();
-//   $('.project').hover(function () {
-//     $('.layover', this).height($('.img', this).height() + 10);
-//     $('.layover', this).fadeIn(300);
-//   }, function () {
-//     $('.layover', this).fadeOut(300);
-//   });
-//   $('#touchscreen').fadeOut();
-//   $(document).scroll(function() {
-//     if ($(this).scrollTop() > 600 && $(window).width() < 800) {
-//       $('#touchscreen').fadeIn();
-//     } else {
-//       $('#touchscreen').fadeOut();
-//     }
-//   });
-// });
+    $( function() {
+        $('.project h2').matchHeight();
+    });
 </script>
 @endsection
 
 @section('footer')
 <footer>&copy; 2016 - Rob Van Keilegom - Vragen of opmerkingen? <a href="mailto:info@robvankeilegom.be"> Mail mij!</a></footer>
 @endsection
+h2
