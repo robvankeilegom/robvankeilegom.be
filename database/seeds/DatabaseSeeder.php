1d9d3c34a8410a814323212deb4e375300ede439
Rob <robvankeilegom@hotmail.com>
2017-05-13 16:21:48 +0200

    remove tagtableseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(ProjectTableSeeder::class);
         $this->call(LinkTableSeeder::class);
-        // $this->call(TagTableSeeder::class);
         $this->call(HeaderDataTableSeeder::class);
     }
 }
