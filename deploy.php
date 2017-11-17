3e802b5321d33c17c875d0bc8adecb51db53455a
Rob <robvankeilegom@hotmail.com>
2017-11-17 17:23:30 +0100

    add deployer

--- deploy.php
+++ deploy.php
@@ -1,190 +1,74 @@
 <?php
 namespace Deployer;
 
-require_once 'recipe/common.php';
-require 'vendor/deployer/recipes/rsync.php';
+require 'recipe/laravel.php';
+require 'recipe/npm.php';
 
-// Configuration
-set('ssh_type', 'native');
-set('ssh_multiplexing', true);
-set('writable_mode', 'chmod');
+// Config
 set('default_stage', 'staging');
 
+// Project name
+set('application', 'robvankeilegom.be');
+
+// Project repository
 set('repository', 'https://RoobieBoobieee@gitlab.com/RoobieBoobieee/Portfolio.git');
 
-// Laravel shared dirs
-set('shared_dirs', [
-    'storage',
-    'vendors',
-    'node_modules',
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+set('env', [
+    'composer_options' => '--verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader',
 ]);
 
-// Laravel shared file
-set('shared_files', [
-    '.env',
-]);
-
-// Laravel writable dirs
-set('writable_dirs', [
-    'bootstrap/cache',
-    'storage',
-    'storage/app',
-    'storage/app/public',
-    'storage/framework',
-    'storage/framework/cache',
-    'storage/framework/sessions',
-    'storage/framework/views',
-    'storage/logs',
-]);
-
-// Servers
-server('production', 'ssh.robvankeilegom.be')
-    ->user('robvankeilegom.be')
-    ->identityFile()
+// Hosts
+host('production')
+    ->hostname('robvankeilegom.be')
     ->set('branch', 'master')
-    ->set('deploy_path', '/home/robvankeilegom.be/robvankeilegom.be')
-    ->pty(true);
+    ->set('deploy_path', '/home/user/web/robvankeilegom.be')
+    ->stage('production');
 
-server('staging', 'ssh.robvankeilegom.be')
-    ->user('robvankeilegom.be')
-    ->identityFile()
-    ->set('branch', 'development')
-    ->set('deploy_path', '/home/robvankeilegom.be/staging.robvankeilegom.be');
+host('staging')
+    ->hostname('staging.robvankeilegom.be')
+    ->set('branch', 'staging')
+    ->set('deploy_path', '/home/user/web/staging.robvankeilegom.be')
+    ->stage('staging');
 
-
-set('rsync_src', __DIR__ . '/vendor');
-set('rsync_dest', '{{release_path}}/vendor');
-
-set('rsync', [
-    'include'      => [],
-    'include-file' => false,
-    'exclude'      => [],
-    'exclude-file' => false,
-    'filter'       => [],
-    'filter-file'  => false,
-    'filter-perdir'=> false,
-    'flags'        => 'rza', // Recursive, with compress
-    'options'      => ['delete'],
-    'timeout'      => 600,
-]);
-
-// task('environment:setup', function () {
-//     run('cp {{deploy_path}}/shared/.env {{release_path}}/.env');
-// })->desc('Environment setup');
-// after('deploy:update_code', 'environment:setup');
-
-
-/**
- * Helper tasks
- */
-desc('Disable maintenance mode');
-task('artisan:up', function () {
-    $output = run('if [ -f {{deploy_path}}/current/artisan ]; then {{bin/php}} {{deploy_path}}/current/artisan up; fi');
-    writeln('<info>' . $output . '</info>');
+// Tasks
+task('build', function () {
+    run('cd {{release_path}} && build');
 });
 
-desc('Enable maintenance mode');
-task('artisan:down', function () {
-    $output = run('if [ -f {{deploy_path}}/current/artisan ]; then {{bin/php}} {{deploy_path}}/current/artisan down; fi');
-    writeln('<info>' . $output . '</info>');
-});
+task('gulp', function () {
+    run("cd {{release_path}} && gulp");
+})->onStage('staging')->desc("Running gulp");
 
-desc('Execute artisan migrate');
-task('artisan:migrate', function () {
-    run('{{bin/php}} {{release_path}}/artisan migrate --force');
-});
+task('gulp:production', function () {
+    run("cd {{release_path}} && gulp --production");
+})->onStage('production')->desc("Running gulp");
 
-desc('Execute artisan migrate:rollback');
-task('artisan:migrate:rollback', function () {
-    $output = run('{{bin/php}} {{release_path}}/artisan migrate:rollback --force');
-    writeln('<info>' . $output . '</info>');
-});
+task('composer:dev', function () {
+    run("cd {{release_path}} && composer install");
+})
+->desc("Running gulp");
 
-desc('Execute artisan migrate:status');
-task('artisan:migrate:status', function () {
-    $output = run('{{bin/php}} {{release_path}}/artisan migrate:status');
-    writeln('<info>' . $output . '</info>');
-});
+before('deploy:vendors', 'composer:dev');
 
-desc('Execute artisan db:seed');
-task('artisan:db:seed', function () {
-    $output = run('{{bin/php}} {{release_path}}/artisan db:seed --force');
-    writeln('<info>' . $output . '</info>');
-});
+// Run npm install
+after('deploy:update_code', 'npm:install');
 
-desc('Execute artisan cache:clear');
-task('artisan:cache:clear', function () {
-    run('{{bin/php}} {{release_path}}/artisan cache:clear');
-});
-
-desc('Execute artisan config:cache');
-task('artisan:config:cache', function () {
-    run('{{bin/php}} {{release_path}}/artisan config:cache');
-});
-
-desc('Execute artisan route:cache');
-task('artisan:route:cache', function () {
-    run('{{bin/php}} {{release_path}}/artisan route:cache');
-});
-
-desc('Execute artisan view:clear');
-task('artisan:view:clear', function () {
-    run('{{bin/php}} {{release_path}}/artisan view:clear');
-});
-
-desc('Execute artisan optimize');
-task('artisan:optimize', function () {
-    run('{{bin/php}} {{release_path}}/artisan optimize');
-});
-
-desc('Execute artisan queue:restart');
-task('artisan:queue:restart', function () {
-    run('{{bin/php}} {{release_path}}/artisan queue:restart');
-});
-
-/**
- * Task deploy:public_disk support the public disk.
- * To run this task automatically, please add below line to your deploy.php file
- *
- *     before('deploy:symlink', 'deploy:public_disk');
- *
- * @see https://laravel.com/docs/5.2/filesystem#configuration
- */
-desc('Make symlink for public disk');
-task('deploy:public_disk', function () {
-    // Remove from source.
-    run('if [ -d $(echo {{release_path}}/public/storage) ]; then rm -rf {{release_path}}/public/storage; fi');
-
-    // Create shared dir if it does not exist.
-    run('mkdir -p {{deploy_path}}/shared/storage/app/public');
-
-    // Symlink shared dir to release dir
-    run('{{bin/symlink}} {{deploy_path}}/shared/storage/app/public {{release_path}}/public/storage');
-});
-
-/**
- * Main task
- */
-desc('Deploy your project');
-task('deploy', [
-    'deploy:prepare',
-    'deploy:lock',
-    'deploy:release',
-    'deploy:update_code',
-    'rsync',
-    'deploy:shared',
-    'deploy:writable',
-    'artisan:view:clear',
-    'artisan:cache:clear',
-    'artisan:config:cache',
-    'artisan:optimize',
-    'artisan:migrate',
-    'deploy:symlink',
-    'deploy:unlock',
-    'cleanup',
-]);
-
-after('deploy', 'success');
+// Run gulp
+after('npm:install', 'gulp:production');
+after('npm:install', 'gulp');
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+before('deploy:symlink', 'artisan:migrate');
