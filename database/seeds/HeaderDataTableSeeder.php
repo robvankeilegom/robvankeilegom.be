8057a8b3bee661765228407cb6131efcd9a0baf3
Rob <robvankeilegom@hotmail.com>
2018-01-18 20:34:30 +0100

    update headerdata

--- database/seeds/HeaderDataTableSeeder.php
+++ database/seeds/HeaderDataTableSeeder.php
@@ -15,9 +15,9 @@ class HeaderDataTableSeeder extends Seeder
         $headerData = App\HeaderData::create([
             'code' => 'MAIN',
             'image' => 'images/profile_picture.jpg',
-            'subtitle' => 'Full Stack Developer',
-            'description1' => "My name is Rob. I'm a 22 year old developer graduated in June 2017. I currently work as a Progress developer over at Organi (Wilrijk, Belgium). In my free time I work on personal or open source projects. When I'm not programming I'm probably busy volunteering at Frag-o-Matic, playing video- and boardgames or out snowboarding.",
-            'description2' => "Even though I don't have that many years of work experience doesn't mean I've not been busy. Programming is my passion as you can see by the many projects I've worked on. Lots of projects in different languages. I love experimenting which is why some projects are unfinished. As I see it, its not about the destination but about the experience gained.",
+            'subtitle' => 'Full Stack Developer - Software Engineer',
+            'description1' => "My name is Rob. I'm a 22 year old developer graduated in June 2017 with a bachelor's degree in Networks, Systems and Security. I currently work as a Progress developer at Organi (Wilrijk, Belgium). In my free time I work on personal or open source projects. When I'm not programming I'm probably busy volunteering at Frag-o-Matic, playing video- and boardgames or out snowboarding.",
+            'description2' => "Even though I don't have that many years of work experience it doesn't mean I've not been busy. Programming is my passion as you can see by the many projects I've worked on: lots of projects in different languages. I love experimenting which is why some projects are unfinished. As I see it, it's not about the destination but about the experience gained.",
         ]);
 
         $links = [
