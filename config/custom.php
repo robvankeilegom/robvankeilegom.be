8cf845df14f4533730fe7106663eaeddf6aee393
Rob Van Keilegom <robvankeilegom@hotmail.com>
2022-03-29 15:28:16 +0200

    :fire: Remove everything

--- config/custom.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'GA' => [
-        'site_key'    => env('GOOGLE_SITE_KEY', ''),
-        'secret_key'  => env('GOOGLE_SECRET_KEY', ''),
-        'g_analytics' => env('GOOGLE_ANALYTICS', ''),
-    ],
-];
